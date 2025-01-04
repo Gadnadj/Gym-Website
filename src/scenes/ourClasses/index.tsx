76f8c0fbad914b8624b65344fbb3e9c00a753c82
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 17:52:27 +0200

    fix bugs responsive

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -6,9 +6,8 @@ import image4 from "@/assets/image4.png";
 import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
 import { motion } from "framer-motion";
-import HText from '@/shared/HTest';
+import HText from "@/shared/HTest";
 import Class from "./Class";
-
 
 const classes: Array<ClassType> = [
     {
@@ -47,54 +46,50 @@ const classes: Array<ClassType> = [
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
-}
+};
 
-const ourClasses = ({ setSelectedPage }: Props) => {
+const OurClasses = ({ setSelectedPage }: Props) => {
     return (
-        <section id="ourclasses" className="w-full bg-primary-100 py-20 ">
+        <section id="ourclasses" className="w-full bg-primary-100 py-40">
             <motion.div
                 onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
             >
                 <motion.div
                     className="mx-auto w-5/6"
-                    initial='hidden'
-                    whileInView='visible'
+                    initial="hidden"
+                    whileInView="visible"
                     viewport={{ once: true, amount: 0.5 }}
-                    transition={{ duration: 1 }}
+                    transition={{ duration: 0.5 }}
                     variants={{
                         hidden: { opacity: 0, x: -50 },
                         visible: { opacity: 1, x: 0 },
-                    }}>
-
+                    }}
+                >
                     <div className="md:w-3/5">
-                        <HText>
-                            OUR CLASSES
-                        </HText>
-                        <p className="py-5"> Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                        <HText>OUR CLASSES</HText>
+                        <p className="py-5">
+                            Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
                             tellus quam porttitor. Mauris velit euismod elementum arcu neque
                             facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
                             enim mattis odio in risus nunc.
                         </p>
                     </div>
                 </motion.div>
-                <motion.div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden"
-                    initial='hidden'
-                    whileInView='visible'
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ delay: 0.2, duration: 1 }}
-                    variants={{
-                        hidden: { opacity: 0, x: -50 },
-                        visible: { opacity: 1, x: 0 },
-                    }}>
+                <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
                     <ul className="w-[2800px] whitespace-nowrap">
-                        {classes.map((item, index) => (
-                            <Class key={`${item.name}-${index}`} name={item.name} description={item.description} image={item.image} />
+                        {classes.map((item: ClassType, index) => (
+                            <Class
+                                key={`${item.name}-${index}`}
+                                name={item.name}
+                                description={item.description}
+                                image={item.image}
+                            />
                         ))}
                     </ul>
-                </motion.div>
+                </div>
             </motion.div>
         </section>
-    )
-}
+    );
+};
 
-export default ourClasses
+export default OurClasses;
