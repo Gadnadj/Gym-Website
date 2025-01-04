b8a988cf52d70b8163f8c213b4795aab65e4671a
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 14:52:40 +0200

    add frame motion

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,8 @@ import HomePageGraphic from '@/assets/HomePageGraphic.png';
 import SponsorRedbull from '@/assets/SponsorRedbull.png';
 import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFortune from '@/assets/SponsorFortune.png';
+import { motion } from 'framer-motion';
+
 
 
 type Props = {
@@ -22,16 +24,25 @@ const Home = ({ setSelectedPage }: Props) => {
                 {/* Main header */}
                 <div className='z-10 mt-32 md:basis-3/5'>
                     {/* Headings */}
-                    <div className='md:-mt-20'>
+                    <motion.div className='md:-mt-20'
+                        initial='hidden'
+                        whileInView='visible'
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 1 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}
+                    >
                         <div className='relative'>
-                            <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-evolvetext'>
+                            <div className='md:before:absolute md:before:-top-20 md:before:-left-20 md:before:z-[-1] md:before:content-evolvetext'>
                                 <img src={HomePageText} alt="home-page-text" />
                             </div>
                         </div>
                         <p className='mt-8 text-sm '>
                             Unrivaled Gym. Unparalleled Training Fitnes Classes. World Class Studios to get the Body Shapes That you Dream of... Get Your Dream Body Now.
                         </p>
-                    </div>
+                    </motion.div>
                     {/* Action */}
                     <div className='mt-8 flex items-center gap-8'>
                         <ActionButton setSelectedPage={setSelectedPage}>
@@ -49,19 +60,28 @@ const Home = ({ setSelectedPage }: Props) => {
                 </div>
 
                 {/* Image */}
-                <div className='flex basis-3/5 justify-center z-10'>
+                <div className='flex basis-3/5 justify-center z-10 md:ml-40 md:mt-16 md:jusiify-items-end'>
                     <img src={HomePageGraphic} alt="home-pageGraphic" />
                 </div>
             </div>
             {/* Sponsors */}
             {isAboveMediumScreens && (
-                <div>
-                    <div>
-                        <div>
+                <div className='h-[150px] w-full bg-primary-100 py-10'>
+                    <div className='mx-auto w-5/6 '>
+                        <motion.div
+                            initial='hidden'
+                            whileInView='visible'
+                            viewport={{ once: true, amount: 0.5 }}
+                            transition={{ duration: 1 }}
+                            variants={{
+                                hidden: { opacity: 0, x: -50 },
+                                visible: { opacity: 1, x: 0 },
+                            }}
+                            className='flex w-full items-center justify-between'>
                             <img src={SponsorRedbull} alt="redbull-sponsor" />
                             <img src={SponsorForbes} alt="forbes-sponsor" />
                             <img src={SponsorFortune} alt="fortune-sponsor" />
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             )}
