22e5961a7c57d0721c714a84e335bae9cc277ec3
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 17:09:52 +0200

    add content part

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,6 +3,10 @@ import { HomeModernIcon, UserCircleIcon, AcademicCapIcon, UserGroupIcon } from "
 import { motion } from "framer-motion";
 import HText from "@/shared/HTest";
 import Benefit from "@/scenes/benefits/Benefits";
+import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
+
+
 
 const benefits: Array<BenefitType> = [
     {
@@ -68,6 +72,50 @@ const Benefits = ({ setSelectedPage }: Props) => {
                         <Benefit key={benefit.title} icon={benefit.icon} title={benefit.title} description={benefit.description} setSelectedPage={setSelectedPage} />
                     ))}
                 </motion.div>
+                {/* Graphics and Description */}
+                <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+                    {/* Graphic */}
+                    <img className="mx-auto" src={BenefitsPageGraphic} alt="benefits-page-graphic" />
+
+                    {/* Description */}
+                    <div>
+                        {/* TItle */}
+                        <div className="relative">
+                            <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                                <div>
+                                    <HText>
+                                        MILLIONS OF HAPPY MEMBERS GETTING{" "} <span className="text-primary-500">FIT.</span>.
+                                    </HText>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Description */}
+                        <div>
+                            <p className="my-5">Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                                Felis orci diam odio.</p>
+                            <p className="mb-5">Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                                enim mattis odio in risus nunc.</p>
+                        </div>
+
+                        {/* Button */}
+                        <div className="relative mt-16">
+                            <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                                <ActionButton setSelectedPage={setSelectedPage}>
+                                    Join Now
+                                </ActionButton>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </motion.div>
         </section>
     )
