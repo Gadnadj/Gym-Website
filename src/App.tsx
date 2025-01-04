78e9312cf5be6ec8b1c282a6a5bf06f63c3e6536
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 16:04:28 +0200

    add benefits page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/scenes/navbar/";
 import Home from "@/scenes/home/";
+import Benefits from "@/scenes/benefits";
+
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -25,6 +27,7 @@ function App() {
     <div className="app bg-gray-20">
       <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,14 @@
+import { SelectedPage } from '@/shared/types'
+import React from 'react'
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Benefits = ({ setSelectedPage }: Props) => {
+    return (
+        <div>Benefits</div>
+    )
+}
+
+export default Benefits
