ce84a8c8990f585bfa9e8a35cbcb0097ababb7a2
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 16:24:02 +0200

    add benefits

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,6 +1,7 @@
 import { SelectedPage } from "@/shared/types";
 import { HomeModernIcon, UserCircleIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+import HText from '@/shared/HTest';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -11,7 +12,20 @@ const Benefits = ({ setSelectedPage }: Props) => {
         <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
             <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
             >
-                <div>
+                {/* Header */}
+                <div className="md:my-5  md:w-3/5">
+                    <HText>
+                        MORE THAN JUST A GYM.
+                    </HText>
+                    <p className="my-5 text-sm">
+                        We provide world class fitness equipment, trainers and classes to
+                        get you to your ultimate fitness goals with ease. We provide true
+                        care into each and every member.
+                    </p>
+                </div>
+
+                {/* Benefits */}
+                <div className="md:flex items-center justify-between gap-8 mt-5">
                     
                 </div>
             </motion.div>
--- src/shared/HTest.tsx
+++ src/shared/HTest.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const HTest = ({ children }: Props) => {
     return (
-        <h1 className="basis-3/5 font-montserrat text-3xl font-bold">
+        <h1 className="basis-3/5 font-montserra text-3xl font-bold">
             {children}
         </h1>
     )
