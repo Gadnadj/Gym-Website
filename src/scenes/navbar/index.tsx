f4509273536a5e1a8be2a025b372d7fd3096d740
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 12:32:58 +0200

    add buttons

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
-import { nav } from "framer-motion/m";
+import { div, nav } from "framer-motion/m";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -12,6 +14,8 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -22,37 +26,48 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
 
           {/* right side */}
-          <div className={`${flexBetween} w-full `}>
-            {/* inner left side */}
-            <div className={`${flexBetween} gap-8 text-sm`}>
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Our Classes"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Benefits"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Contact Us"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-            </div>
+          {isAboveMediumScreens ? (
+            <div className={`${flexBetween} w-full `}>
+              {/* inner left side */}
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
 
-            {/* inner right side */}
-            <div className={`${flexBetween} gap-8`}>
-              <p>Sign In</p>
-              <button>Become a Member</button>
+              {/* inner right side */}
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Become a Member
+                </ActionButton>
+              </div>
             </div>
-          </div>
+          ) : (
+            <button
+              className="rounded-full bg-secondary-500 p-2"
+              onClick={() => setIsMenuToggled(!isMenuToggled)}
+            >
+              <Bars3Icon className="h-6 w-6 text-white" />
+            </button>
+          )}
         </div>
       </div>
     </nav>
