42cc345dc249bc982840ca2c8adb79f3bc116d5d
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 13:27:24 +0200

    add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,18 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier"; // Ajout du plugin Prettier
+import eslintConfigPrettier from "eslint-config-prettier"; // Ajout de la config Prettier
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "plugin:prettier/recommended", // Active la règle Prettier
+      eslintConfigPrettier, // Désactive les règles de formatage d'ESLint
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +23,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      prettier, // Ajout du plugin Prettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -24,7 +32,8 @@ export default tseslint.config(
         "error",
         { allowObjectTypes: true },
       ],
-
+      "semi": ["error", "always"], // Exige des points-virgules
+      "prettier/prettier": ["error"], // Applique les règles de Prettier dans ESLint
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,3 +1,8 @@
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
+  semi: true, // Assurez-vous que Prettier ajoute des points-virgules
+  singleQuote: true, // Utilisation de guillemets simples
+  trailingComma: "all", // Ajouter des virgules de fin dans les objets et tableaux
+  tabWidth: 2, // Définir la largeur d'indentation (par exemple, 2 espaces)
+  useTabs: false, // Utiliser des espaces au lieu des tabulations
 };
