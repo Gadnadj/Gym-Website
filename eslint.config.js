c4580622aa12ef7637c9918565a60e13f1070df1
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 13:37:09 +0200

    add navbar for mobile

--- eslint.config.js
+++ eslint.config.js
@@ -2,42 +2,47 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-plugin-prettier"; // Ajout du plugin Prettier
-import eslintConfigPrettier from "eslint-config-prettier"; // Ajout de la config Prettier
+import eslintConfigPrettier from "eslint-config-prettier"; // Configuration Prettier
+import prettier from "eslint-plugin-prettier"; // Plugin Prettier
+import typescript from "@typescript-eslint/eslint-plugin"; // Plugin TypeScript
+import typescriptParser from "@typescript-eslint/parser"; // Parser TypeScript
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      "plugin:prettier/recommended", // Active la règle Prettier
-      eslintConfigPrettier, // Désactive les règles de formatage d'ESLint
-    ],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      prettier, // Ajout du plugin Prettier
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-empty-object-type": [
-        "error",
-        { allowObjectTypes: true },
-      ],
-      "semi": ["error", "always"], // Exige des points-virgules
-      "prettier/prettier": ["error"], // Applique les règles de Prettier dans ESLint
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
+export default {
+  parser: typescriptParser, // Utilisation du parser TypeScript
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module", // Pour utiliser les imports ES6
   },
-);
+  extends: [
+    js.configs.recommended, // Configuration ESLint de base
+    "plugin:@typescript-eslint/recommended", // Recommandations TypeScript
+    "plugin:react-hooks/recommended", // Recommandations React Hooks
+    "plugin:react-refresh/recommended", // Recommandations React Refresh
+    "plugin:prettier/recommended", // Active la règle Prettier
+    eslintConfigPrettier, // Désactive les règles de formatage d'ESLint
+  ],
+  files: ["**/*.{ts,tsx}"],
+  languageOptions: {
+    globals: globals.browser,
+  },
+  plugins: {
+    "react-hooks": reactHooks,
+    "react-refresh": reactRefresh,
+    prettier, // Ajout du plugin Prettier
+    "@typescript-eslint": typescript,
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-empty-object-type": [
+      "error",
+      { allowObjectTypes: true },
+    ],
+    "semi": ["error", "always"], // Exige des points-virgules
+    "prettier/prettier": ["error"], // Applique les règles de Prettier dans ESLint
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+  ignorePatterns: ["dist"], // Ignorer les fichiers générés
+};
