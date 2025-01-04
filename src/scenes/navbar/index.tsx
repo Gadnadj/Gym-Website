1b3591ae0944682ba8526248c008529c4a0c1cab
Gad Nadjar <gadnadjar@gmail.com>
2025-01-04 12:46:31 +0200

    add icons

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -23,53 +23,64 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           {/* left side */}
           <div className={`${flexBetween} w-full gap-16`}>
             <img src={Logo} alt="logo" />
+            {/* right side */}
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full `}>
+                {/* inner left side */}
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+
+                {/* inner right side */}
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
-
-          {/* right side */}
-          {isAboveMediumScreens ? (
-            <div className={`${flexBetween} w-full `}>
-              {/* inner left side */}
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact Us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-              </div>
-
-              {/* inner right side */}
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <ActionButton setSelectedPage={setSelectedPage}>
-                  Become a Member
-                </ActionButton>
-              </div>
-            </div>
-          ) : (
-            <button
-              className="rounded-full bg-secondary-500 p-2"
-              onClick={() => setIsMenuToggled(!isMenuToggled)}
-            >
-              <Bars3Icon className="h-6 w-6 text-white" />
-            </button>
-          )}
         </div>
       </div>
+
+      {/* Mobile Menu Modal */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-x">
+          {/* Close Icon */}
+          <div className="justify-end p-12 flex">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
